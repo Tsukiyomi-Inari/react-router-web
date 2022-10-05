61f1799cd9bf3d909c12541e1d30974959923722
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-10-05 17:54:28 -0400

    start of project

---
+++ src/components/index.jsx
@@ -0,0 +1,10 @@
+export { default as Navigation } from "./Navigation";
+export { default as Footer } from "./Footer";
+export { default as Home } from "./Home";
+
+//********     Pages       *********/
+export { default as Classwork } from "./Classwork";
+export { default as Project } from "./Project";
+export { default as Labs } from "./labs/Lab";
+export { default as Posts } from "./labs/Lab_Posts";
+export { default as Post } from "./labs/Post";
