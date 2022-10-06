154c1e30118a0f28b4695972eccd76eacdd191a8
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-10-05 23:14:48 -0400

    main set up complete

--- src/components/Classworks.jsx
+++ src/components/Classworks.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BlueWindow from "./assets/BlueWindow";
 
 function Classworks() {
   return (
@@ -6,14 +7,10 @@ function Classworks() {
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
+            <BlueWindow />
           </div>
           <div class="col-lg-5">
-            <h1 class="font-weight-light">About</h1>
+            <h1 class="font-weight-light">Classworks</h1>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
