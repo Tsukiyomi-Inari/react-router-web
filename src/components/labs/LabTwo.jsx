d4e4e3463debdf7b5f3ab0889670eb7b616bb43e
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-10-11 13:43:51 -0400

    modified

--- src/components/labs/LabTwo.jsx
+++ src/components/labs/LabTwo.jsx
@@ -12,7 +12,7 @@ function LabTwo() {
           </div>
           <div className="col-lg-5">
          <h2>Lab 2</h2>
-      <a href="./lab2-table.pdf"target="_blank">Mobile Development Buildtool: Android Studio Specs table</a>
+      <a href="https://github.com/Tsukiyomi-Inari/react-router-web/raw/master/src/components/labs/lab2-table.pdf"target="_blank" rel="noreferrer">Mobile Development Buildtool: Android Studio Specs table</a>
       <p><strong>Description:</strong> ::comming soon::</p>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/x0o5k0wke9w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
       
