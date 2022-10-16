f773cfba2a799b41b0fb88186d30c17111c17073
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-10-16 18:26:12 -0400

    footer alignment change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     <div className="footer">
       <footer className="py-2  fixed-bottom opacity-50">
         <div className="container">
-          <p className="m-0 text-center text-muted">
+          <p className="m-0 text-start text-muted">
             Copyright &copy; <a href="https://github.com/Tsukiyomi-Inari" target="_blank" rel="noreferrer">Katherine Bellman  {new Date().getFullYear()} </a>
           </p>
         </div>
