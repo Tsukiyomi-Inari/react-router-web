b4557ece09d2631c7230cf04818be28fbb0ba771
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-10-16 18:21:11 -0400

    start of lab1 doc insert changes

--- src/components/labs/LabOne.jsx
+++ src/components/labs/LabOne.jsx
@@ -10,10 +10,40 @@ function LabOne() {
           <div className="col-lg-6 align-self-start">
           <BlueWindowLabs />
           </div>
-          <div className="col-lg-5 text-center ">
-         <h1><span className="kat">Lab 1</span></h1>
-      <p><strong>Description:</strong> ::comming soon::</p>
-      <a href="https://github.com/Tsukiyomi-Inari/react-router-web/raw/master/src/components/labs/Lab1-MODE4201-Bellman.pdf" target="_blank" rel="noreferrer">Download Lab 1 Report</a>
+          <div className="col-lg-5  ">
+         <h1 className="text-center"><span className="kat ">Lab 1</span></h1>
+      <p className="text-center"><strong>Description:</strong><a href="https://github.com/Tsukiyomi-Inari/react-router-web/raw/master/src/components/labs/Lab1-MODE4201-Bellman.pdf" target="_blank" rel="noreferrer">Download Lab 1 Report</a> ::comming soon::</p>
+      <br/>
+      <p>
+        <h6 className="text-center">Mobile Phone History: Past, Present and Future</h6>
+<br/>
+	&nbsp;&nbsp;Over 30 years since Motorola developed, "the bone," which boasted a talk time of 30 minutes, was approved for market. The device was approximately a foot long and weighed around eight times  more than the devices we use now (Films Media Group,2015). The history of mobile phones is what has brought us to the present and also into the future. All of which can only be fully understood by starting from the beginning and working forward. <br/>
+ <br/>
+<strong>Past: 1980 – 2006</strong><br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;At the start of a cellphone production began in 1984 when Motorola introduced the first "commercial handheld cellular phone," the DynaTAC 800X, nicknamed "the brick." Phones remained bulky for quite some time, But by the 90s, they had lost some weight. Flip phones that could be carried in a pocket became the norm. Still, the other format was I'm much thinner- a more minor variation of its predecessor. In 2002 cameraphones dominated the market, and this added functionality increased consumer consumption (Boston Globe, 2014). Both the flip phone and the camera phones were sought by the younger generation, who began to popularize the use of texting.<br/><br/>
+<strong>Present: Post-2008</strong><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;In 2007 Apple released the first iPhone, a little bulky compared to modern devices but much closer in design (Boston Globe, 2014). The device featured a touch screen, replacing the previously commonplace dial pad buttons. New functionalities included a Music player, web browsing, games, a personal calendar and many more. Smartphone. The new format has coined the smartphone, creating a unique niche within the mobile phone market.<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;Smartphone innovation continues to grow today with the addition of other technologies, and functionality from installable applications (Rhoads, 2005). They have become the modern replacements for PDA devices. Many computing activities can be completed on the pocket-sized device in the palm of your hand.
+<br/><br/>
+<strong >Future</strong><br/>
+	 &nbsp;&nbsp;Mobile phone innovations continue to; we are at one time communicating across distances, which Was only seen in movies and TV shows such as Star Trek. <br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;The future possesses unlimited possibilities for innovation and the development of mobile phones. Some technologies, such as AI, virtual reality, and augmented reality, are just a few Of the results from the minds of engineers. Each possesses the possibility of further development within its own niche and possible extension through sci-fi technologies. This includes ideas such as brainwave control, gesture control, and even holograms (Films Media Goup, 2015). Additional engineering and refinement are needed for these. Still, they are possible future components in the next generation of mobile phones.
+</p>
+      <div>
+        <strong className="text-center">References</strong> <br/> 
+        <div className="">
+        <p> J. (2003, Sep 15). A phone with a history. New Statesman, 16, R6-R7. Retrieved from http://search.proquest.com.dproxy.library.dc-uoit.ca/magazines/phone-with-history/docview/224365213/se-2.
+Buy Galaxy A70 128GB online at Best Price | Samsung Singapore. (2021, May 10). Samsung Sg. Retrieved September 15, 2022, from https://www.samsung.com/sg/smartphones/galaxy-a/galaxy-a70-blue-128gb-sm-a705mzbwxsp/buy/ .</p>
+<p>Films Media Group. (2015, November 6). History of the Mobile Phone [Documentary Film]. Films On Demand; Retreived. https://fod.infobase.com/PortalPlaylists.aspx?wID=102530&xtid=9420 .
+Heckerling, Amy.(1995) Cher (Alicia Silverstone) and friends holding cellphones. From Clueless[Film]. Paramount Pictures. Retreived from https://www.zimbio.com/A+Comprehensive+History+of+Cell+Phones+in+Movies/articles/LYXjNMDDhK2/The+Social+Network .</p>
+<p>Best Buy Canada. Home. (n.d.).[Photograph] Retrieved September 15, 2022, from https://www.bestbuy.ca/en-ca/product/samsung-galaxy-a70-a705w-128gb-open-box-black/14695620?cmp=knc-s-71700000081849102&amp;gclid=Cj0KCQjwmouZBhDSARIsALYcourjvF5EoWf14cezh3BBMRk7cjgrSmW2Px-tfmHhpQjk-bjo4CSjRaAaAj6xEALw_wcB&amp;gclsrc=aw.ds   </p>
+<p>Virgin Plus. (n.d.). Mobile Phones, Plans, Internet & More. Retrieved September 22, 2022, from https://www.virginplus.ca/en/home/index.html  </p>
+<p>Rhoads, C. (2005, Jan 04). Cellphones Become 'Swiss Army Knives' as Technology Blurs. Wall Street Journal http://search.proquest.com.dproxy.library.dc-uoit.ca/newspapers/cellphones-become-swiss-army-knives-as-technology/docview/398897738/se-2.   </p>
+<p> Samsung Galaxy A70 - Full phone specifications. (n.d.). Retrieved September 22, 2022, from https://www.gsmarena.com/samsung_galaxy_a70-9646.php .  </p>
+<p>Stone, Oliver. (1987).Gordon Gecko (Michael Douglas) holding "the brick". From Wall Street [Film]. American Entertainment Partners Amercent Films. Retreived from https://www.rediscoverthe80s.com/2018/06/rediscovering-brick-cellular-phone-from_7.html  </p>
+</div>
+
+      </div>
     
           </div>
            
